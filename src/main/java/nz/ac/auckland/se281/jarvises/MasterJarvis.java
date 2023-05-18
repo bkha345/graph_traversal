b1d9b0337c5743e7e0139176a22c2b6159407899
bkha345 <bkha345@aucklanduni.ac.nz>
2023-05-19 10:09:56 +1200

    more fomratting fixes

--- src/main/java/nz/ac/auckland/se281/jarvises/MasterJarvis.java
+++ src/main/java/nz/ac/auckland/se281/jarvises/MasterJarvis.java
@@ -2,33 +2,33 @@ package nz.ac.auckland.se281.jarvises;
 
 import java.util.ArrayList;
 import nz.ac.auckland.se281.jarvises.strategies.Strategy;
-import nz.ac.auckland.se281.jarvises.strategies.StrategyRandom;
 import nz.ac.auckland.se281.jarvises.strategies.StrategyAverage;
+import nz.ac.auckland.se281.jarvises.strategies.StrategyRandom;
 import nz.ac.auckland.se281.jarvises.strategies.StrategyTop;
 
 public class MasterJarvis implements Jarvis {
 
-    private Strategy strategy;
+  private Strategy strategy;
 
-    public MasterJarvis() {
-        this.strategy = new StrategyRandom();
+  public MasterJarvis() {
+    this.strategy = new StrategyRandom();
+  }
+
+  @Override
+  public int[] generateFingerAndSum(ArrayList<Integer> userFingerInputs, int roundsDone) {
+
+    // alternates between average and top after 3 rounds
+    if (roundsDone >= 3) {
+      if (roundsDone % 2 == 1) {
+        this.strategy = new StrategyAverage();
+      } else {
+        this.strategy = new StrategyTop();
+      }
     }
 
-    @Override
-    public int[] generateFingerAndSum(ArrayList<Integer> userFingerInputs, int roundsDone) {
-
-        // alternates between average and top after 3 rounds
-        if (roundsDone >= 3) {
-            if (roundsDone % 2 == 1) {
-                this.strategy = new StrategyAverage();
-            } else {
-                this.strategy = new StrategyTop();
-            }
-        }
-
-        // returns array of finger and sum
-        action[0] = strategy.generateFinger();
-        action[1] = strategy.generateSum(userFingerInputs, roundsDone, action[0]);
-        return action;
-    }
+    // returns array of finger and sum
+    action[0] = strategy.generateFinger();
+    action[1] = strategy.generateSum(userFingerInputs, roundsDone, action[0]);
+    return action;
+  }
 }
--- src/main/java/nz/ac/auckland/se281/jarvises/MediumJarvis.java
+++ src/main/java/nz/ac/auckland/se281/jarvises/MediumJarvis.java
@@ -2,30 +2,30 @@ package nz.ac.auckland.se281.jarvises;
 
 import java.util.ArrayList;
 import nz.ac.auckland.se281.jarvises.strategies.Strategy;
-import nz.ac.auckland.se281.jarvises.strategies.StrategyRandom;
 import nz.ac.auckland.se281.jarvises.strategies.StrategyAverage;
+import nz.ac.auckland.se281.jarvises.strategies.StrategyRandom;
 
 public class MediumJarvis implements Jarvis {
 
-    private Strategy strategy;
+  private Strategy strategy;
 
-    // does random strategy for first 3 rounds
-    public MediumJarvis() {
-        this.strategy = new StrategyRandom();
+  // does random strategy for first 3 rounds
+  public MediumJarvis() {
+    this.strategy = new StrategyRandom();
+  }
+
+  @Override
+  public int[] generateFingerAndSum(ArrayList<Integer> userFingerInputs, int roundsDone) {
+
+    // switches to average strategy after 3 rounds
+    if (roundsDone >= 3) {
+      this.strategy = new StrategyAverage();
     }
 
-    @Override
-    public int[] generateFingerAndSum(ArrayList<Integer> userFingerInputs, int roundsDone) {
-
-        // switches to average strategy after 3 rounds
-        if (roundsDone >= 3) {
-            this.strategy = new StrategyAverage();
-        }
-
-        // returns array of finger and sum
-        action[0] = strategy.generateFinger();
-        action[1] = strategy.generateSum(userFingerInputs, roundsDone, action[0]);
-        return action;
-    }
+    // returns array of finger and sum
+    action[0] = strategy.generateFinger();
+    action[1] = strategy.generateSum(userFingerInputs, roundsDone, action[0]);
+    return action;
+  }
 
 }
